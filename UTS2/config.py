58e6524e7be919d05422027233c349b9fc5bee3a
kucingcoklat512 <123432009+kucingcoklat512@users.noreply.github.com>
2024-11-09 14:33:37 +0900

    Update config.py

--- UTS2/config.py
+++ UTS2/config.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://dbhotel_fillscreen:79455f36812f1a2a169ad8b61a480452cb0e4c17@9780d.h.filess.io:3305/dbhotel_fillscreen'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector:/dbhotel2_headedtall:d000b1fc0c1d13802ec156ab36cdf4fb9974c428@glcpd.h.filess.io:3305/dbhotel2_headedtall'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
